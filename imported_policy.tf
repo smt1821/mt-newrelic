b10cdab7560fc5c62779a86ea849da8a5d2dfe8e
smt1821 <merictabakoglu@gmail.com>
2025-03-02 11:53:55 +0000

    imported

---
+++ imported_policy.tf
@@ -0,0 +1,7 @@
+# newrelic_alert_policy.imported_policy:
+resource "newrelic_alert_policy" "imported_policy" {
+    account_id          = 3737312
+    id                  = "868940"
+    incident_preference = "PER_POLICY"
+    name                = "Mail"
+}
--- main.tf
+++ main.tf
@@ -2,3 +2,12 @@ resource "newrelic_alert_policy" "cpu_alert_policy" {
   name                = "CPU Usage Policy"
   incident_preference = "PER_POLICY"  # Options: PER_POLICY, PER_CONDITION, PER_CONDITION_AND_TARGET
 }
+
+###IMPORTING THE EXISTING RESOURCE###
+resource "newrelic_alert_policy" "imported_policy" {
+  name                = "Placeholder"  # This will be updated after import
+  incident_preference = "PER_POLICY"   # This will also be updated
+}
+
+
+
